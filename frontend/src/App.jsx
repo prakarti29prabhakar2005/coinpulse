7f2bc3fe7d3c92e2488801f01a1457c7206bbfa2
ramveermeena2003 <ramveermeena2003@gmail.com>
2026-02-24 19:52:14 +0530

    Implemented responsive header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Header from "./components/Common/Header"
+import Footer from "./components/Common/Footer"
+
+
+function App() {
+  return <div className='App'>
+    <Header />
+    {/* <Footer /> */}
+  </div>
+}
+
+export default App
---
+++ frontend/src/components/Common/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './styles.css'
+import AnchorTemporaryDrawer from './drawer.jsx'
+
+const Header = () => {
+  return (
+    <div className='navbar'>
+      <h1 className='logo'>Coinpulse<span style={{color: "var(--blue)"}}>.</span></h1>
+      <div className='links'>
+        <a href="/">
+        <p className='link'>Home</p>
+        </a>
+        <a href="/">
+        <p className='link'>Compare</p>
+        </a>
+        <a href="/">
+        <p className='link'>Watchlist</p>
+        </a>
+        <a href="/">
+        <p className='link'>Dashboard</p>
+        </a>
+      </div>
+      
+      <div className='mobile-drawer'>
+        <AnchorTemporaryDrawer/>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
